cbf2424d30988ca17f4489b9b8154da897de0c4a
einwille <einwil01@ads.uni-passau.de>
2021-09-07 13:34:44 +0200

    use stopwords from custom module

--- lib/nlp_yt/sw.py
+++ lib/nlp_yt/sw.py
@@ -1,6 +1,7 @@
 ''' module exports static content '''
 
 from nltk.corpus import stopwords
+# nltk.download('stopwords')
 
 stop_words_add = [
     "mal",
@@ -62,9 +63,13 @@ stop_words_add = [
 ]
 
 def get_stop_words_add():
-    ''' return stop_words_add'''
+    ''' return stop_words_add list'''
     return stop_words_add
 
+def get_stop_words_nltk():
+    ''' return german nltk stopwords set'''
+    return set(stopwords.words("german"))
+
 def get_stop_words():
-    ''' return german stopwords -- union of german nltk stopwords and stop_words_add'''
-    return list(set(stopwords.words("german"))) + get_stop_words_add()
+    ''' return german stopwords set -- union of german nltk stopwords and stop_words_add'''
+    return set(list(set(stopwords.words("german"))) + list(set(get_stop_words_add())))
